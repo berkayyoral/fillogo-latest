8982df2a180e18f97b123f639378d01adc30cf30
melikegmstkn <melikegmstkn@gmail.com>
2024-06-10 15:56:02 +0300

    onesignal paketini günceller

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import OneSignalFramework
 import Flutter
 import GoogleMaps
 
@@ -10,6 +11,11 @@ import GoogleMaps
   ) -> Bool {
       GeneratedPluginRegistrant.register(with: self);
       GMSServices.provideAPIKey("AIzaSyAFFLTo2huCvR-HiQTkDIpjJ5Yb-b-erN8");
+    OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+        
+        // OneSignal initialization
+    OneSignal.initialize("43ddc771-f2eb-4ecf-bcc8-95434809b1dc", withLaunchOptions: launchOptions)
+        
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
